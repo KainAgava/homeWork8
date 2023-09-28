322bb19f44e6213e6507880a026392b1d5f29bae
KainAgava <143312166+KainAgava@users.noreply.github.com>
2023-09-28 22:06:33 +0700

    Add files via upload

---
+++ homeWork2.1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWork2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int valeu = rand.Next(1, 1000);
+            Console.WriteLine("случайная генерация числе, когда надоест введите exit ");
+            Console.WriteLine($"например {valeu}, можете попробовать угадать следующее число ");
+            string userUnput = Console.ReadLine();
+            string exit =  "exit";
+
+            while (exit != userUnput)
+                if (exit == userUnput)
+                {
+                    Console.WriteLine(exit);
+                    break;
+                }
+                else
+                {
+                    valeu = rand.Next(1, 1000);
+                    Console.WriteLine(valeu);
+                    Console.WriteLine("Какое число будет следующим? ");
+                    userUnput = Console.ReadLine();
+                }
+        }
+    }
+    
+}
